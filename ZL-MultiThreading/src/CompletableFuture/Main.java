0dea110621a7fc7a6081fde517824ebd67a92835
Aniruddh Nagar <anagar@zlti.com>
2026-02-03 17:37:29 +0530

    Learning ThreadPool / Future / Completable

--- ZL-MultiThreading/src/CompletableFuture/Main.java
+++ ZL-MultiThreading/src/CompletableFuture/Main.java
@@ -1,11 +1,15 @@
 package CompletableFuture;
 
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Collections.sort;
 
 //Chaining Operations: You can chain multiple asynchronous operations seamlessly.
     //thenApply(): Executes a function when the previous stage completes.
